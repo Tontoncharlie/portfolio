9aacc217242d7925636d8a336901505b347fbc25
Tontoncharlie <ugochukwucharles1418@gmail.com>
2025-05-08 17:51:03 +0100

    response

--- src/app/Components/Navbar/Navbar.js
+++ src/app/Components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
 
       <div
         className={styles.hamburger}
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => {setIsOpen(!isOpen)}}
         tabIndex={0}
         role="button"
         onKeyPress={(e) => e.key === 'Enter' && setIsOpen(!isOpen)}
@@ -22,12 +22,12 @@ const Navbar = () => {
       </div>
 
       <div className={`${styles.navLinks} ${isOpen ? styles.showMenu : ''}`}>
-        <Link href="/"><h1 tabIndex={0}>HOME</h1></Link>
-        <Link href="/about"><h1 tabIndex={0}>ABOUT</h1></Link>
-        <Link href="/resume"><h1 tabIndex={0}>RESUME</h1></Link>
-        <Link href="/services"><h1 tabIndex={0}>SERVICES</h1></Link>
-        <Link href="/portfolio"><h1 tabIndex={0}>PORTFOLIO</h1></Link>
-        <Link href="/contact"><h1 tabIndex={0}>CONTACT</h1></Link>
+        <Link href="/"><h1 tabIndex={0} onClick={() => {setIsOpen(!isOpen)}}>HOME</h1></Link>
+        <Link href="/about"><h1 tabIndex={0} onClick={() => {setIsOpen(!isOpen)}}>ABOUT</h1></Link>
+        <Link href="/resume"><h1 tabIndex={0} onClick={() => {setIsOpen(!isOpen)}}>RESUME</h1></Link>
+        <Link href="/services"><h1 tabIndex={0} onClick={() => {setIsOpen(!isOpen)}}>SERVICES</h1></Link>
+        <Link href="/portfolio"><h1 tabIndex={0} onClick={() => {setIsOpen(!isOpen)}}>PORTFOLIO</h1></Link>
+        <Link href="/contact"><h1 tabIndex={0} onClick={() => {setIsOpen(!isOpen)}}>CONTACT</h1></Link>
       </div>
     </nav>
   );
