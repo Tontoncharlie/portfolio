0d59bd2f6055d3968fa19732e862b3787bc5ab1d
Tontoncharlie <ugochukwucharles1418@gmail.com>
2025-05-08 12:20:11 +0100

    fixed skill css

--- src/app/Components/skills/Skills.js
+++ src/app/Components/skills/Skills.js
@@ -1,6 +1,6 @@
 'use client';
 import { motion } from 'framer-motion';
-import styles from './Skills.module.css';
+import styles from './skills.module.css';
 
 const skills = [
   { name: 'HTML', level: 95 },
